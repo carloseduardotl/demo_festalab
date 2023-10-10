ac7e980015560a07d9ddf562e47dcc811ed67636
Carlos Eduardo Taborda Lottermann <cadutaborda@gmail.com>
2023-10-10 14:48:33 -0300

    Added a phone validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
 
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
     validates_cpf_format_of :cpf
+    validates :phone, format: { with: /\A\([1-9]{2}\) (?:[2-8]|9[0-9])[0-9]{3}\-[0-9]{4}\z/, message: "Phone should be (xx) xxxxx-xxxx" }
 
 end
